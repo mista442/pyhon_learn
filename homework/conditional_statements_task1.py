797feca1937b3ec0824eb9b544e06f1072e5104d
root <Tubaltor@mail.ru>
2023-11-18 01:18:26 +0300

    fix task1

--- homework/conditional_statements_task1.py
+++ homework/conditional_statements_task1.py
@@ -17,10 +17,29 @@ while flag:
         flag=False
 
 import random
-rand_bigger=0
-rand_less=0
-
 rand1 = random.randint(1,100)
-print(rand1)
 rand2 = random.randint(1,100)
-print(rand2)
+
+l=[]
+l.append(rand1)
+l.append(rand2)
+l.sort()
+
+flag1=True
+while flag1:
+    if a > l[0] and a < l[1]:
+        print(a,"находится в заданном диапазоне")
+    else:
+        flag1=False
+    if b > l[0] and b < l[1]:
+        print(b,"находится в заданном диапазоне")
+    else:
+        flag1=False
+    if c > l[0] and c < l[1]:
+        print(c,"находится в заданном диапазоне")
+    else:
+        flag1=False
+    if (a < l[0] or a > l[1]) and (b < l[0] or b > l[1]) and (c < l[0] or c > l[1]):
+        print("ни одно из чисел пользователя не попало в отрезок")
+    else:
+        flag1=False

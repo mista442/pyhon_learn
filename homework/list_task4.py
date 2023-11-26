9e30361e83701579ce68c49bd03dae07b0c18637
Mikhail Mit <mista442@gmail.com>
2023-11-26 10:31:02 +0300

    draft list 4 task

--- homework/list_task4.py
+++ homework/list_task4.py
@@ -1,9 +1,12 @@
 # Задача 4 Поменять местами самый большой и самый маленький элементы списка
 
-a = [1,4,1,2,8,10,4,5]
+a = [10,1,4,1,2,8,10,4,5]
 min1=min(a)
 max1=max(a)
 
-print(min1)
-print(a.index(min1))
-print(max1)
+#print(min1)
+#print(a.index(min1))
+#print(max1)
+
+for i in a:
+    print(a.index(i))

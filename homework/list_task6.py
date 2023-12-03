eb9fcf30fb8518de80707aa8f5c678262e59ca1b
root <Tubaltor@mail.ru>
2023-12-03 20:02:55 +0300

    draft list task 6

--- homework/list_task6.py
+++ homework/list_task6.py
@@ -5,15 +5,28 @@
 
 a=[[1,2,3],[2,6,4],[3,4,5],[6,3,7]]
 print(a)
-print(a[0][0])
+b=[] # пустой список куда буду складывать только уникальные числа
 
-b=[]
-outside_index = 0
-inside_index = 0
+list_count_indexes = len(a) # сколько всего вложенных листов в общем списке
+count_next_index = 0
 
-for i in b:
-    if a[0][inside_index] not in b:
-        b.append(a)
-    inside_index += 1
 
-print(b)
+count_index = 0 # счетчик для одного индекса подписков - [1,2,3] - первый индекс списка, [2,6,4] - второй индекс списка и т.д.
+el_count = 0 # счетчик для элементов внутри вложенного списка
+
+next_podspisok = 0 # счетчик для элементов в каждом нового списка
+
+count_el_podspisok = len(a[next_podspisok])
+
+while el_count < count_el_podspisok:
+    print(a[count_index][el_count])
+    if a[count_index][el_count] not in b:
+        b.append(a[count_index][el_count])
+    el_count += 1
+    if el_count == count_el_podspisok:
+        el_count = 0
+        count_index +=1
+        next_podspisok +=1
+        if count_next_index == list_count_indexes:
+            break
+    print(b)

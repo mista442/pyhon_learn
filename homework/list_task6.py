96ef71174210915148dc72ccca89381e6ee2ba84
root <Tubaltor@mail.ru>
2023-12-03 20:37:39 +0300

    list task 6

--- homework/list_task6.py
+++ homework/list_task6.py
@@ -5,11 +5,12 @@
 
 a=[[1,2,3],[2,6,4],[3,4,5],[6,3,7]]
 print(a)
+
 b=[] # пустой список куда буду складывать только уникальные числа
+c=[] # пустой список куда буду складывать повторяющиеся элементы
 
 list_count_indexes = len(a) # сколько всего вложенных листов в общем списке
-count_next_index = 0
-
+count_next_index = 0 # сколько перебрали индексов
 
 count_index = 0 # счетчик для одного индекса подписков - [1,2,3] - первый индекс списка, [2,6,4] - второй индекс списка и т.д.
 el_count = 0 # счетчик для элементов внутри вложенного списка
@@ -19,7 +20,14 @@ next_podspisok = 0 # счетчик для элементов в каждом н
 count_el_podspisok = len(a[next_podspisok])
 
 while el_count < count_el_podspisok:
-    print(a[count_index][el_count])
+    if count_next_index == list_count_indexes:
+        break
+#    print(a[count_index][el_count])
+    if a[count_index][el_count] in b:
+        if a[count_index][el_count] in c:
+            pass
+        else:
+            c.append(a[count_index][el_count])
     if a[count_index][el_count] not in b:
         b.append(a[count_index][el_count])
     el_count += 1
@@ -27,6 +35,7 @@ while el_count < count_el_podspisok:
         el_count = 0
         count_index +=1
         next_podspisok +=1
-        if count_next_index == list_count_indexes:
-            break
-    print(b)
+        count_next_index +=1
+
+print("\nуникальные элементы из списка:",b)
+print("повторяющиеся элементы из списка:",c)

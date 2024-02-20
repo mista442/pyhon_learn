76f31200cf7852dce1e4ad99ef35c3af5307603d
root <Tubaltor@mail.ru>
2024-02-20 23:31:48 +0300

    def task4

--- homework/def/task4.py
+++ homework/def/task4.py
@@ -2,29 +2,47 @@
 число. Например, если дан список [1, 1, 3, 2, 1, 3, 4], то в нем число 1 встречается три раза,
 число 3 - два раза, числа 2 и 4 - по одному разу."""
 
-# создать пустой список, добавить в него уникальные элементы из изначального списка
-# уникальный список переделать в словарь и в качестве значения ложить количество элементов из изначального списка
+# код без функции
 
-#надо добавить сортировку сначала
+# l = [1, 1, 3, 2, 1, 3, 4]
+# l.sort()
+# print(l)
+# l1 = [] # создаем уникальный лист куда будем складывать только уникальный элементы
+# for i in l:
+#     if i not in l1:
+#         l1.append(i)
+# print(l1)
+#
+# for i in l1:
+#     print("число",i,"встречается",l.count(i),"раз")
 
-l = [1, 1, 3, 2, 1, 3, 4]
-#def count_num():
-#    global l
-# c=0
-# # l_uniq = []
-dict1 = {}
-l1 = []
+# код с функциями
 
-for i in l:
-    if i not in l1:
-        l1.append(i)
-    #if i == l.index(i-1):
-    #    continue
-print(l1)
-    # l.count(i)
-    # c = c + 1
-    #print("число",i,"встречается",l.count(i),"раз")
+import random
+def GenLists():
 
-c=0
-for i in l1:
-    print(l.count(l1.count(i)))
+        l=[]
+        n=int(input("укажите количество элементов в списке: "))
+
+        for i in range(n):
+                a=random.randint(1,4)
+                l.append(a)
+        l.sort() # сортируем полученный список
+        return l
+
+list = GenLists()
+print(list) # отображаем полученный сгенериррованный список
+
+def Get_uniq_items():
+    l_uniq = []
+    for i in list:
+        if i not in l_uniq:
+            l_uniq.append(i)
+    return l_uniq
+list_uniq = Get_uniq_items()
+#print(list_uniq)
+
+def Print_count_uniq_items():
+    for i in list_uniq:
+        print("число",i,"встречается",list.count(i),"раз")
+Print_count_uniq_items()
